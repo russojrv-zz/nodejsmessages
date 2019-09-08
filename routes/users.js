18b2516e2554b86be10c885c8366382a9a098cbb
Jorge Russo Vizcaíno <russo.jrv@gmail.com>
2019-09-08 21:07:28 +0200

    Añadido proyecto

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var redis = require('redis');
+var client = redis.createClient(4000, "http://storage");
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+
+  client.on('connect', function() {
+    res.send("Conectado");
+  });
+
+});
+
+module.exports = router;
