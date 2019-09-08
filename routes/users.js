9a04a867a81ddf7dcc8837e73003b6cb67003ff9
Jorge Russo Vizcaíno <russo.jrv@gmail.com>
2019-09-08 22:09:34 +0200

    last tried to change redis host

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var redis = require('redis');
-var client = redis.createClient(4000, "172.17.0.3");
+var client = redis.createClient("redis://storage:4000");
 var router = express.Router();
 
 /* GET users listing. */
